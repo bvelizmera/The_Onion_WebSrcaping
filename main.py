f6b5b7b16c4ba379888124fdf1661efb871d1b6a
Bill <bvelizmera@gmail.com>
2024-11-27 15:41:56 +0000

    finished get_artciles_from_page

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import requests as req
 from bs4 import BeautifulSoup
 from datetime import datetime
+from time import sleep
+import json
 
 class OnionError(Exception):
     """Error acessing the Onion site."""
@@ -18,8 +20,8 @@ def get_article_details(url:str) -> dict:
     onion_soup = BeautifulSoup(result.text, features = "html.parser")
 
     tag_holder = onion_soup.find("div", class_= "taxonomy-post_tag")
-    tags = tag_holder.find_all("a")
-    
+    tags = tag_holder.find_all("a") if tag_holder else []
+
     return {
         "title" : onion_soup.find("h1").get_text(),
         "published" : datetime.fromisoformat(onion_soup.find("time")["datetime"]),
@@ -27,8 +29,30 @@ def get_article_details(url:str) -> dict:
     }
 
 def get_articles_from_page(url:str) -> list[dict]:
-    pass
+
+    result = req.get(url)
+
+    if result.status_code >= 400:
+        raise OnionError("Could not access URL successfully.")
+
+    onion_soup = BeautifulSoup(result.text, features="html.parser")
+
+    links = onion_soup.find_all("h3")
+    articles = []
+    for l in links:
+        articles.append(get_article_details(l.find("a")["href"]))
+
+    return articles
+
+
+    
+    
 
 
 if __name__ == "__main__":
-    print(get_article_details("https://theonion.com/quentin-tarantino-breaks-three-day-media-silence-1819586098/"))
+    # print(get_article_details("https://theonion.com/quentin-tarantino-breaks-three-day-media-silence-1819586098/"))
+    all_articles = []
+    for i in range(1, 10):
+        all_articles.extend( get_articles_from_page(f"https://theonion.com/news/page/{i}"))
+    
+    
